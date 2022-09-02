15c81fae38f033a8b37de0c856ea38c155a47c12
Alexandr <don.alexande1986@yandex.ru>
2022-09-02 05:40:49 +0300

    Задача 48

--- Tasks/Task 48/Program.cs
+++ Tasks/Task 48/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = m + n;
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 3;
+int n = 4;
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
